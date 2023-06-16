276ac7dd7e364f95532ba12c56eda42593ffb2ac
clintonodia <odiamehiclinton@gmail.com>
2023-06-16 15:13:17 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry for abc
+ * Return: Always 0
+ */
+int main(void)
+{
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        putchar(ch);
+      putchar('\n');
+    }
+    return (0);
+}
