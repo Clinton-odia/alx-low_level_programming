b3a0146a41c9250d2fc19a8b688b43cf3d768079
clintonodia <odiamehiclinton@gmail.com>
2023-06-16 17:00:25 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers to be printed
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numz;
+
+	for (numz = 0; numz > 10 ; numz++)
+	{
+		putchar((numz % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
