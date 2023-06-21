1a25ad80fa09ac9a7a9d623eb9cf71139b822dc0
clintonodia <odiamehiclinton@gmail.com>
2023-06-20 18:05:45 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @d: to be calculated
+ * Return: return absolute value
+ */
+int _abs(int d)
+{
+	if (d > 0)
+	{
+		return (d);
+	}
+	else
+	{
+		return (-d);
+	}
+}
