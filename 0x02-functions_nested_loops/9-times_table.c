7d6ffa45f56048c7133606f981746ffcc94a560a
clintonodia <odiamehiclinton@gmail.com>
2023-06-20 19:41:28 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table.
+ * @number: number to be multiple
+ * @multiple: number to multiple
+ * @product: product of multiplication
+ * Return: no return
+ */
+void times_table(void)
+{
+	int number, multiple, product;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+
+		for (multiple = 1; multiple <= 9; multiple++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = number * multiple;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
