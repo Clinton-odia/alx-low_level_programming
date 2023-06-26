ac7296f875d7a20e8df86892eefc08bef47b4a2d
clintonodia <odiamehiclinton@gmail.com>
2023-06-26 15:11:53 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string.
+ * @str: The string been printed.
+ */
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (str[len])
+		len++;
+
+	for (i = (len + 1) / 2; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
